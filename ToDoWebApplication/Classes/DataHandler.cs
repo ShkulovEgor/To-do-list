407334a9ebbcd3ccf0a2f26bb84dbc48ab95e267
ShkulovEgor <Shkulov987@mail.ru>
2021-06-24 12:20:29 +0700

    Загрузка проекта.

---
+++ ToDoWebApplication/Classes/DataHandler.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ToDoWebApplication.Models;
+using System.IO;
+
+namespace ToDoWebApplication.Classes
+{
+    static public class DataHandler
+    {
+        //получить все задачи
+        static public List<Task> GetAllTasks(string databasePath)
+        {
+            List<Task> tasks = new List<Task>();
+
+            try
+            {
+                //построчное чтение
+                using (StreamReader sr = new StreamReader(databasePath, System.Text.Encoding.GetEncoding(1251)))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Task task = new Task();
+
+                        //разделить строку на составляющие по запятым
+                        string[] data = line.Split(',');
+
+                        //пропустить первую строку
+                        if (data[0] == "id")
+                            continue;
+
+                        //пропустить пустые строки
+                        if (data.Length != 6)
+                            continue;
+
+                        task.Id = Convert.ToInt32(data[0]);
+                        task.Name = data[1];
+                        task.Description = data[2];
+                        task.StartDate = Convert.ToDateTime(data[3]);
+                        task.EndDate = Convert.ToDateTime(data[4]);
+                        task.File = data[5];
+
+                        tasks.Add(task);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+            return tasks;
+        }
+
+        //записать задачи
+        static public void SaveTasks(string databasePath, List<Task> tasks)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(databasePath, false, System.Text.Encoding.UTF8))
+                {
+                    sw.WriteLine("id,name,description,startdate,enddate,file");
+
+                    foreach (Task task in tasks)
+                        sw.WriteLine(GetLineForDatabase(task));
+                }
+            }
+            catch (Exception ex)
+            {
+                string exception = ex.ToString();
+            }
+        }
+
+        //получить задачу по ID
+        static public Task GetTaskByID(int id, string dbPath)
+        {
+            List<Task> tasks = GetAllTasks(dbPath);
+
+            foreach (Task tsk in tasks)
+                if (tsk.Id == id)
+                {
+                    Task task = new Task();
+
+                    task.Id = tsk.Id;
+                    task.Name = tsk.Name;
+                    task.Description = tsk.Description;
+                    task.StartDate = tsk.StartDate;
+                    task.EndDate = tsk.EndDate;
+                    task.File = tsk.File;
+
+                    return task;
+                }
+
+            return null;
+        }
+
+        //редактировать задачу по ID
+        static public void EditTask(int id, string dbPath, string name, string description, DateTime enddate)
+        {
+            List<Task> tasks = GetAllTasks(dbPath);
+
+            foreach (Task task in tasks)
+            {
+                if (task.Id == id)
+                {
+                    task.Name = name;
+                    task.Description = description;
+                    task.EndDate = enddate;
+                }
+            }
+
+            SaveTasks(dbPath, tasks);
+        }
+        
+        //удалить задачу по ID
+        static public void DeleteTask(int id, string dbPath)
+        {
+            List<Task> tasks = GetAllTasks(dbPath);
+
+            int index = -1;
+            //найти индекс удаляемого элемента
+            for (int i = 0; i < tasks.Count; i++) 
+                if (tasks[i].Id == id)
+                {
+                    index = i;
+                    break;
+                }
+
+            if (index > -1)
+                tasks.RemoveAt(index);
+
+            SaveTasks(dbPath, tasks);
+        }
+
+        //добавить задачу
+        static public void AddTask(Task task, string dbPath)
+        {
+            List<Task> tasks = GetAllTasks(dbPath);
+            task.Id = GetNewID(dbPath);
+            tasks.Add(task);
+            SaveTasks(dbPath, tasks);
+        }
+
+        //сгенерировать ID для новой задачи
+        static private int GetNewID(string dbPath)
+        {
+            List<Task> tasks = GetAllTasks(dbPath);
+            if (tasks.Count == 0)
+                return 0;
+            else 
+                return tasks.Last().Id + 1;
+        }
+
+        //сформировать из задачи строку
+        static private string GetLineForDatabase(Task task)
+        {
+            string line = "";
+
+            line = line + task.Id.ToString() + ',' + task.Name + ',' + task.Description + ',' 
+                            + task.StartDate.ToString("dd.MM.yyyy") + ',' + task.EndDate.ToString("dd.MM.yyyy") 
+                            + ',' + task.File;
+
+            return line;
+        }
+
+        //выполнение поиска
+        static public List<Task> SearchTasks(string query, string dbPath)
+        {
+            //перевести поисковой запрос в нижний регистр
+            query = query.ToLower();
+
+            //слова поискового запроса
+            string[] queryWords = query.Split(' ');
+
+            //полный список задач
+            List<Task> tasks = GetAllTasks(dbPath);
+
+            //соответствующие запросу
+            List<Task> filteredTasks = new List<Task>();
+
+            foreach(Task task in tasks)
+            {
+                //соответствует запросу
+                bool flag = false;
+
+                //есть ли слова запроса в названии или описании
+                for (int i = 0; i < queryWords.Length; i++)
+                {
+                    if (task.Name.ToLower().Contains(queryWords[i]))
+                    {
+                        flag = true;
+                        break;
+                    }
+                    if (task.Description.ToLower().Contains(queryWords[i]))
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+
+                //соответствует - переносится в результат
+                if (flag == true)
+                    filteredTasks.Add(task);
+            }
+
+            return filteredTasks;
+        }
+    }
+}
---
+++ ToDoWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoWebApplication.Classes;
+using ToDoWebApplication.Models;
+
+namespace ToDoWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        static private string dbPath;
+
+        /*Главная страница*/
+        public ActionResult Index()
+        {
+            dbPath = Server.MapPath("~/Data/database.csv");
+
+            List<Task> tasks = DataHandler.GetAllTasks(dbPath);
+
+            if (tasks == null)
+            {
+                ViewBag.Error = "Не найдено задач";
+                tasks = new List<Task>();
+            }
+            else
+                ViewBag.Tasks = tasks;
+
+            if (TempData["Error"] != null)
+                ViewBag.Error = TempData["Error"].ToString();
+
+            return View();
+        }
+
+        /*Добавить задачу*/
+        [HttpGet]
+        public ActionResult AddTask()
+        {
+            return View();
+        }
+
+        /*Сохранение добавленной задачи*/
+        [HttpPost]
+        public ActionResult SaveTask(HttpPostedFileBase upload, string name, string description, DateTime enddate)
+        {
+            try
+            {
+                if ((name == "") || (description == "") || (enddate == Convert.ToDateTime("01.01.2021")))
+                {
+                    TempData["Error"] = "Были введены не все поля";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    DateTime startdate = DateTime.Now;
+
+                    string dbPath = Server.MapPath("~/Data/database.csv");
+
+                    //получение имени файла
+                    string filename = Path.GetFileName(upload.FileName);
+                    //сохранение файла на сервер
+                    upload.SaveAs(Server.MapPath("~/Files/" + filename));
+
+                    Task task = new Task();
+                    task.Name = name;
+                    task.Description = description;
+                    task.StartDate = startdate;
+                    task.EndDate = enddate;
+                    task.File = filename;
+                    DataHandler.AddTask(task, dbPath);
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = "Не удалось загрузить файл: " + ex.ToString();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        /*Скачивание файла*/
+        [HttpGet]
+        public FileResult Download(string id)
+        {
+            //получение файла по идентификатору
+            string filePath = DataHandler.GetTaskByID(Convert.ToInt32(id), dbPath).File;
+
+            //загрузка файла
+            return GetFile(filePath);
+        }
+
+        /*Получение файла для скачивания из хранилища на сервере*/
+        private FileResult GetFile(string filePath)
+        {
+            try
+            {
+                //путь к файлу
+                string filepath = Server.MapPath("~/Files/" + filePath);
+                //тип файла
+                string filetype = "application/pdf";
+                //имя файла
+                string filename = filePath;
+
+                return File(filepath, filetype, filename);
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = "Не удалось загрузить файл: " + ex.ToString();
+                return null;
+            }
+        }
+
+        /*Редактирование информации о выбранном файле*/
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            if (id != null)
+            {
+                try
+                {
+                    //найти задачу по идентификатору
+                    Task task = DataHandler.GetTaskByID(Convert.ToInt32(id), dbPath);
+
+                    //передать задачу в представление
+                    ViewBag.Task = task;
+                }
+                catch (Exception ex)
+                {
+                    TempData["Error"] = "Произошла ошибка: " + ex.ToString();
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                TempData["Error"] = "Пустой идентификатор.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        /*Сохранение изменения информации о задаче*/
+        [HttpPost]
+        public ActionResult SaveChanges(string id, string name, string description, DateTime enddate)
+        {
+            if (id != null)
+            {
+                try
+                {
+                    if ((name == "") || (description == "") || (enddate == Convert.ToDateTime("01.01.2021")))
+                    {
+                        TempData["Error"] = "Были введены не все поля";
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        //отредактировать задачу
+                        DataHandler.EditTask(Convert.ToInt32(id), dbPath, name, description, enddate);
+
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    TempData["Error"] = "Произошла ошибка: " + ex.ToString();
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                TempData["Error"] = "Пустой идентификатор.";
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        /*Удаление задачи*/
+        [HttpPost]
+        public ActionResult Delete(string id)
+        {
+            if (id != null)
+            {
+                try
+                {
+                    //нахождение файла задачи
+                    string filename = DataHandler.GetTaskByID(Convert.ToInt32(id), dbPath).File;
+
+                    //путь к файлу
+                    string filepath = Server.MapPath("~/Files/" + filename);
+
+                    //нахождение файла на хранилище сервера
+                    FileInfo fileInfo = new FileInfo(filepath);
+
+                    if (fileInfo.Exists)
+                    {
+                        //удаление на сервере
+                        fileInfo.Delete();
+                    }
+
+                    //удаление задачи
+                    DataHandler.DeleteTask(Convert.ToInt32(id), dbPath);
+                }
+                catch (Exception ex)
+                {
+                    TempData["Error"] = "Произошла ошибка: " + ex.ToString();
+                }
+            }
+            else
+            {
+                TempData["Error"] = "Произошла ошибка.";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        /*Выполнение поиска*/
+        [HttpGet]
+        public ActionResult Search(string query)
+        {
+            if (query != null)
+            {
+                //все задачи, удовлетворяющие запросу
+                List<Task> tasks = DataHandler.SearchTasks(query, dbPath);
+
+                //передать результат в представление
+                if (tasks.Count != 0)
+                {
+                    ViewBag.Tasks = tasks;
+                }
+
+                return View();
+            }
+            else
+            {
+                TempData["Error"] = "Пустой запрос.";
+                return RedirectToAction("Index");
+            }
+        }
+
+    }
+}
